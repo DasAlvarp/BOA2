349afeb643e35371f60fa3312f2f9a439a22f52c
DasAlvarp <alvarogudis@gmail.com>
2015-10-11 01:05:52 -0500

    Initial commit

---
+++ src/BOA.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Alvarp on 10/11/2015.
+ */
+public class BOA
+{
+    public static final int TIME = 2000;
+
+    private Queue EventQueue = new LinkedList<EventItem>();
+
+    public static void main(String[] args)
+    {
+        for(int clock = 0; clock < TIME; clock++)
+        {
+
+        }
+    }
+}
---
+++ src/EventItem.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Alvarp on 10/11/2015.
+ */
+public class EventItem
+{
+    public int time_of_day;
+    public int service_time;
+    public int type_of_event;
+
+}
